a489996882a5f26cae518a42c49a616ff0d8d246
Mohammed Shahabaz <81280603+mdshahabaz@users.noreply.github.com>
2023-07-18 20:56:15 +0530

    Add files via upload

---
+++ notification-service/src/main/java/com/programming/techie/orderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.programming.techie;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class orderPlacedEvent {
+    private String orderNumber;
+}
